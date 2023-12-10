1ca956f5ef374fbff84761308bac9cd57a57a0ea
ai408 <913292836@qq.com>
2023-12-10 15:19:28 +0800

    目录结构与公众号一致

--- Llama2-Chinese/inference-speed/GPU/vllm_example/single_gpu_api_server.sh
+++
@@ -1,5 +0,0 @@
-# /mnt/data/zhangzheng/data/atomgpt/model_sft_atomgpt_28000_0613/checkpoint-12000_merge
-
-CUDA_VISIBLE_DEVICES=0 python api_server.py \
---model "/mnt/data_online/models/llama/models--meta-llama--Llama-2-7b-chat-hf" \
---port 8090
--- Llama2-Chinese/train/merge_peft_model/merge.sh
+++
@@ -1,5 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python merge_peft_adapter.py \
-    --adapter_model_name /checkpoint-2200 \
-    --output_name checkpoint-2200_merge \
-    --load8bit false \
-    --tokenizer_fast false  
